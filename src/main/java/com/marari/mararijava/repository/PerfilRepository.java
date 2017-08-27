d8ce11cde8f43b9210c1cdfdb14421dcf82b7667
Matheus Vilvert <matheusvilvert@gmail.com>
2017-08-27 15:13:13 -0300

    login, crud - tipo despesa e usuario

---
+++ src/main/java/com/marari/mararijava/repository/PerfilRepository.java
@@ -0,0 +1,7 @@
+package com.marari.mararijava.repository;
+
+import com.marari.mararijava.model.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil,Integer> {
+}
